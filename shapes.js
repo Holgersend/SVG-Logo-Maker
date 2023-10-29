930c7fc885166ed827bbb18f2fbb3d04794a7b09
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-29 17:03:47 -0500

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -12,3 +12,17 @@ class circle {
         return `<circle cx="150" cy="115" r="80" fill="${this.color}" />`;
     };
 };
+
+class Square {
+    render(){
+        return `<rect x="50" height="200" width="200" fill="${this.color}">`
+    }
+}
+
+class Triangle extends Shape{
+    render(){
+        return `<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.color}">`
+    }
+};
+
+module.exports = {Circle, Square, Triangle}
