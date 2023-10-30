e2da54b7db1c127ad5394128566af1610bf686e5
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-29 20:52:25 -0500

    edited index.js
    instance for the svg

--- input.js
+++ input.js
@@ -7,17 +7,38 @@ const fs = require("fs");
                 type: 'input',
                 name: 'text',
                 message: 'Enter the text up to 3 characters for the logo:',
+                validate: (nameInput) => {
+                    if (nameInput.length <= 3) {
+                    return true;
+                    } else {
+                    console.log("Please enter a logo that is 3 characters or less!");
+                    }
+                },
             },
             {
                 type: 'input',
                 name: 'textColor',
                 message: 'Enter a color keyword or a hexadecimal number for the text (e.g, blue, yellow, #f33CEFF, etc.)',
+                validate: (nameInput) => {
+                    if (nameInput) {
+                    return true;
+                    } else {
+                    console.log("Please enter a color for your logo text!");
+                    }
+                },
             },
             {
                 type: 'list',
                 name: 'shape',
                 message: 'Select a shape for the logo:',
                 choices: ['Circle', 'Triangle', 'Square'],
+                validate: (nameInput) => {
+                    if (nameInput) {
+                    return true;
+                    } else {
+                    console.log("Please choose a shape!");
+                    }
+                },
             },
             {
                 type: 'input',
