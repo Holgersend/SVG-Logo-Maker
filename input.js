d516941124d1e5cf8b2fe4c6490199a5da8cc9be
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-10-29 20:59:41 -0500

    edited shapes.js
    set base parameters for Shape class

--- input.js
+++ input.js
@@ -44,7 +44,47 @@ const fs = require("fs");
                 type: 'input',
                 name: 'shapeColor',
                 message: 'Enter a color keyword or a hexadecimal number for the shape (e.g, blue, yellow, #f33CEFF, etc.)',
+                validate: (nameInput) => {
+                    if (nameInput) {
+                    return true;
+                    } else {
+                    console.log("Please enter a color!");
+                    }
+                },
+                
             }
         ];
 
-module.exports = {input};
+        function generateSVG(data) {
+            let shape
+            switch (data.shape) {
+                case 'Circle':
+                    shape = new shapes.Circle(data.text, data.shapeColor)
+                    break
+                case 'Triangle':
+                    shape = new shapes.Triangle(data.text, data.shapeColor)
+                    break
+                case 'Square':
+                    shape= new shapes.Square(data.text, data.shapeColor)
+                    break
+            }
+            
+            let svg = 
+            `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+            <svg width="200" height="300" xmlns="http://www.w3.org/2000/svg">
+                ${shape.render()}
+                <text x="50%" y="50%" font-size="4.5vw" dominant-baseline="middle" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
+            </svg>`;
+
+            fs.writeFile('./examples/logo.svg', svg, (err) => {
+                if (err) throw err;
+                console.log('Generated logo.svg');
+            })
+        }
+        function init() {
+            inquirer.prompt(input).then(data => {
+                generateSVG(data)
+            })
+        }
+        
+        init()
--- shapes.js
+++ shapes.js
@@ -1,27 +1,25 @@
 class Shape {
-    constructor() {
-        this.color = '';
-    }
-    setColor(color) {
-        this.color = (color);
+    constructor(text, shapeColor) {
+        this.text = text;
+        this.shapeColor = shapeColor;
     }
 }
 
 class Circle  extends Shape{
     render(){
-        return `<circle cx="150" cy="115" r="80" height="100%" width="100%" fill="${this.color}" />`;
+        return `<circle cx="150" cy="115" r="80" height="100%" width="100%" fill="${this.shapeColor}" />`;
     };
 };
 
 class Square extends Shape {
     render(){
-        return `<rect x="50" height="200" width="200" fill="${this.color}">`
+        return `<rect x="50" height="200" width="200" fill="${this.shapeColor}">`
     }
 }
 
 class Triangle extends Shape{
     render(){
-        return `<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.color}">`
+        return `<polygon height="100%" width="100%" points="0,200 300,200 150,0" fill="${this.shapeColor}">`
     }
 };
 
